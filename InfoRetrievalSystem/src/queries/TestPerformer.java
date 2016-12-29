7bc4b23e0d3691a2f85a1c3d4c0ef3d08b9edc5a
TimL <timleenaers@gmail.com>
2016-12-29 14:27:33 +0100

    Extra examples queries

--- InfoRetrievalSystem/src/queries/TestPerformer.java
+++ InfoRetrievalSystem/src/queries/TestPerformer.java
@@ -7,7 +7,12 @@ public class TestPerformer {
 	public static void main(String[] args) {
 		try {
 			QueryPerformer performer = new QueryPerformer();
+			// Proximity Query (~ 4 = within 4 words)
 			performer.setQuery("\"22 CA581203\"~4");
+			// Normal query
+			//performer.setQuery("CA581203");
+			// Wildcard
+			//performer.setQuery("CA*");
 			performer.results();
 		} catch (IOException e) {
 			e.printStackTrace();
